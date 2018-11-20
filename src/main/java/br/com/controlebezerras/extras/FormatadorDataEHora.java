562971da05249d262b5a8716bde6fdade94d52f2
joao-scanagatta <joaoscanagatta1@hotmail.com>
2018-11-20 08:33:15 -0200

    commit

--- src/main/java/br/com/controlebezerras/extras/FormatadorDataEHora.java
+++ src/main/java/br/com/controlebezerras/extras/FormatadorDataEHora.java
@@ -3,8 +3,22 @@ package br.com.controlebezerras.extras;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import org.joda.time.DateTime;
+
 //uso da API Joda-Time
 public class FormatadorDataEHora {
+
+	public static String dataAtual() {
+		DateTime dd = new DateTime();
+		String diaAtual = dd.toString("dd/MM/yyyy");
+		return diaAtual;
+	}
+
+	public static String horaAtual() {
+		DateTime dh = new DateTime();
+		String horaAtual = dh.toString("HH:mm:ss");
+		return horaAtual;
+	}
 
 	public static String formatarData(LocalDate data) {
 		String vazio = "";
