30cdb0582e1bd49c0a229b89d706c28a20516c6f
joao-scanagatta <joaoscanagatta1@hotmail.com>
2018-12-06 01:19:09 -0200

    commit

--- src/main/java/br/com/controlebezerras/HttpsConfiguration.java
+++ src/main/java/br/com/controlebezerras/HttpsConfiguration.java
@@ -11,7 +11,6 @@ public class HttpsConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// Customize the application security
 		http.requiresChannel().anyRequest().requiresSecure();
-		http.authorizeRequests();
 	}
 
 }
